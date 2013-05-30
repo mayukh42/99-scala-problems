916697b86f9b92e468c49e53205373c6bcc55277
mayukh71 <mayukh.mukherjee@gmail.com>
2013-05-31 04:35:55 +0530

    arithmetic problems (functions only)

--- src/my99_arith.scala
+++ src/my99_arith.scala
@@ -1,3 +1,112 @@
 object my99_arith {
+  
+  def isPrime(n: Int): Boolean = {
+    /**@NOTES: Fermat's Little Theorem implementation is
+     * limited by ln(Int.MAX_VALUE): 
+     * return (n > 1) && (((1<<n) - 2) % n == 0)
+     * The below one is trivial O(n**0.5)*/ 
+    if (n < 2) return false
+    val limit = Math.pow(n, 0.5).toInt
+    for (i <- 2 to limit) {
+      if ((n % i) == 0) return false
+    }
+    return true
+  }
+  
+  def gcd(m: Int, n: Int): Int = {
+    /**@NOTES: Euler's GCD Algorithm */
+    if (n < 1) m
+    else if (m > n) gcd(n, m-n)
+    else gcd(m, n-m)
+  }
+  
+  def isCoprimeTo(m: Int, n: Int): Boolean = gcd(m, n) == 1
+  
+  /** @NOTES: i until j is an IterableSequence, not a List. */
+  def factors(n: Int): List[Int] = (2 until n).filter(x => n % x ==0).toList
+  
+  def totient(n: Int): Int = {
+    def tot_rec(a: Int, m: Int, acc: List[Int]): List[Int] = {
+      if (a > m) acc
+      else if (gcd(a, m) == 1) tot_rec(a+1, m, a::acc)
+      else tot_rec(a+1, m, acc)
+    }
+    val ans = tot_rec(1, n, List())
+    ans.size
+  }
+  
+  /** @NOTES: running iters upto n**0.5 would not catch some prime factors on the other
+   *  side of sqrt, i.e. if the number is a product of 2 primes on either side of the sqrt */
+  def primeFactorsSet(n: Int): List[Int] = factors(n).filter(x => n%x ==0 && isPrime(x)).toList
+  
+  def primeFactors(n: Int): List[Int] = {
+    val limit = Math.pow(n, 0.5).toInt
+    def pf_rec(x: Int, m: Int, acc: List[Int]): List[Int] = {
+      if (x > limit ) acc
+      else if (m%x == 0 && isPrime(x)) {
+        val y = m/x
+        if (isPrime(y) && y > limit) pf_rec(2, y, x::y::acc)
+        else pf_rec(2, y, x::acc)
+      }
+      else pf_rec(x+1, m, acc)
+    }
+    pf_rec(2, n, List()).sortBy(x => x)
+  }
+  
+  def primeFactorsMultiplicity(n: Int): List[(Int, Int)] = {
+    val ans = primeFactors(n).groupBy((x: Int) => x) 
+    /** @NOTES: O(n) + O(n) + O(nlgn) = O(nlgn) */
+    ans.map(x => (x._1, (x._2).size)).toList.sortBy(x => x._1) 
+  }
+  
+  def totientBetter(n: Int): Int = {
+    val ps = primeFactorsMultiplicity(n)
+    /** @NOTES: phi(m) = (p1-1)*p1**(m1-1) * (p2-1)*p2**(m2-1) * (p3-1)*p3**(m3-1) * ... 
+     * valid only for composites; doesn't make sense for primes as totient = n-1
+     *  */
+    def tb_rec(xs: List[(Int, Int)], phi: Int): Int = {
+      xs match {
+        case (p, m)::ys => tb_rec(ys, phi * (p-1) * Math.pow(p, m-1).toInt)
+        case Nil => phi
+      }
+    }
+    tb_rec(ps, 1)
+  }
+  
+  def listPrimesInRange(a: Int, b: Int): List[Int] = {
+    def lpr_rec(x: Int, y: Int, acc: List[Int]): List[Int] = {
+      if (x > y) acc
+      else if (isPrime(x)) lpr_rec(x+1, y, x::acc)
+      else lpr_rec(x+1, y, acc)
+    }
+    lpr_rec(a, b, List()).reverse
+  }
+  
+  def goldbach(n: Int): (Int, Int) = {
+    if (n > 2 && (n % 2 == 0)) {
+      for (i <- 2 to n/2) {
+        if (isPrime(i) && isPrime(n-i)) return (i, n-i)
+      }
+    } 
+    return (-1, -1)
+  }
+  
+  def goldbachPairs(xs: Range): List[(Int, (Int, Int))] = {
+    val ys = xs.filter(x => x % 2 == 0)
+    ys.map(y => (y, goldbach(y))).toList
+  }
+  
+  def goldbachPairsLimited(xs: Range, lowermost: Int): List[(Int, (Int, Int))] = 
+    goldbachPairs(xs).filter(x => x._2._1 > lowermost)
+  
+  /** @NOTES: to check for function running times */
+  def timer[A](f: => A) = {
+    val t = System.nanoTime()
+    val ans = f
+    println("time: " + (System.nanoTime()-t)/1e6 + " ms")
+    ans
+  }
+  
+  
 
 }
--- src/my99_arith_suite.scala
+++ src/my99_arith_suite.scala
@@ -2,9 +2,95 @@ import org.scalatest.FunSuite
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 
-import my99_lists._
+import my99_arith._
 
 @RunWith(classOf[JUnitRunner])
 class my99_arith_suite extends FunSuite {
+  
+  test("p31: isPrime") {
+    assert(isPrime(7))
+    assert(!isPrime(1))
+    assert(!isPrime(28))
+    assert(isPrime(29))
+  }
+  
+  test("p32: gcd") {
+    assert(gcd(21, 14)==7)
+    assert(gcd(1, 7)==1)
+    assert(gcd(36, 63)==9)
+    assert(gcd(200, 1)==1)
+  }
+  
+  test("p34: totient") {
+    assert(totient(10)==4)
+    assert(totient(20)==8)
+    assert(totient(30)==8)
+    assert(totient(41)==40)
+  }
+  
+  test("p35a: primeFactorsSet") {
+    assert(primeFactorsSet(10)==List(2,5))
+    assert(primeFactorsSet(5)==List())
+    assert(primeFactorsSet(64)==List(2))
+    assert(primeFactorsSet(30)==List(2,3,5))
+  }
+  
+  test("p35b: primeFactors") {
+    assert(primeFactors(315)==List(3,3,5,7))
+    assert(primeFactors(5)==List())
+    assert(primeFactors(64)==List(2,2,2,2,2,2))
+    assert(primeFactors(30)==List(2,3,5))
+  }
+  
+  test("p36: primeFactorsMultiplicity") {
+    assert(primeFactorsMultiplicity(315)==List((3,2), (5,1), (7,1)))
+    assert(primeFactorsMultiplicity(5)==List())
+    assert(primeFactorsMultiplicity(64)==List((2,6)))
+    assert(primeFactorsMultiplicity(56)==List((2,3), (7,1)))
+    assert(primeFactorsMultiplicity(10)==List((2,1), (5,1)))
+  }
+  
+  test("p37: totientBetter") {    
+    assert(totientBetter(10)==4)
+    assert(totient(20)==totientBetter(20))
+    assert(totient(30)==totientBetter(30))
+    assert(totient(96)==totientBetter(96))
+  }
+  
+  test("p38: totient compares") {
+    /** @NOTES: 30x improvement for totientBetter. Truly better! 
+     *  Best: 11.0 ms, 0.32 ms */
+    timer(totient(10090))
+    timer(totientBetter(10090))
+  }
+  
+  test("p39: listPrimesInRange (both inclusive)") {    
+    assert(listPrimesInRange(7, 31)==List(7, 11, 13, 17, 19, 23, 29, 31))
+    assert(listPrimesInRange(1, 1)==List())
+    assert(listPrimesInRange(1, 2)==List(2))
+    assert(listPrimesInRange(6, 14)==List(7, 11, 13))
+  }
+  
+  test("p40: goldbach") {
+    assert(goldbach(10)==(3,7))
+    assert(goldbach(20)==(3,17))
+    assert(goldbach(28)==(5,23))
+    assert(goldbach(96)==(7,89))
+  }
+  
+  test("p41a: goldbachPairs") {    
+    assert(goldbachPairs(9 to 20)==List((10,(3,7)), (12,(5,7)), (14,(3,11)), (16,(3,13)), (18,(5,13)), (20,(3,17))))
+    assert(goldbachPairs(2 to -3)==List())
+  }
+  
+  test("p41b: goldbachPairsLimited") {    
+    assert(goldbachPairsLimited(1 to 2000, 50)==List((992,(73,919)), (1382,(61,1321)), (1856,(67,1789)), (1928,(61,1867))))
+    assert(goldbachPairsLimited(2001 to 5000, 100)==List((2642,(103,2539))))
+    assert(goldbachPairsLimited(2001 to 5000, 60)==List((2078,(61,2017)), (2438,(61,2377)), (2618,(61,2557)), (2642,(103,2539)), (3458,(67,3391)), (3794,(61,3733)), (3818,(79,3739)), (3848,(79,3769)), (4322,(61,4261)), (4418,(61,4357)), (4508,(61,4447)), (4618,(71,4547)), (4712,(61,4651)), (4718,(61,4657)), (4784,(61,4723)), (4886,(73,4813))))
+    /** @NOTES: times for 3000, 48000, 498000 numbers (ceil(timer)):
+    timer(goldbachPairsLimited(2001 to 5000, 60))		// 30 ms
+    timer(goldbachPairsLimited(2001 to 50000, 60))		// 243 ms
+    timer(goldbachPairsLimited(2001 to 500000, 60))		// 3276 ms */
+  }
 
 }
